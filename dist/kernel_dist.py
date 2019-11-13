950accddc9ded38854dd10adf16eaa7d8e460547
youhanxia <youhan.xia@gmail.com>
2019-11-13 17:58:59 +1100

    Correct Graph object initialisation

--- dist/kernel_dist.py
+++ dist/kernel_dist.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
-from grakel import kernels
-from grakel import Graph
+from grakel import GraphKernel, Graph
 
 from dist import Abstract_Dist
 from problems import Abstract_Prob, TSP
@@ -9,21 +8,21 @@ from problems import Abstract_Prob, TSP
 
 class Kernel_Dist(Abstract_Dist):
 
-    def dist(self, inst_a: Abstract_Prob, inst_b: Abstract_Prob, kernel='MultiscaleLaplacianFast'):
+    def dist(self, inst_a: Abstract_Prob, inst_b: Abstract_Prob, kernel='shortest_path'):
 
-        g_a = Graph(squareform(pdist(inst_a)))
-        g_b = Graph(squareform(pdist(inst_b)))
+        g_a = Graph(squareform(pdist(inst_a)), node_labels=list(np.zeros(inst_a.n, dtype=int)))
+        g_b = Graph(squareform(pdist(inst_b)), node_labels=list(np.zeros(inst_a.n, dtype=int)))
 
         # add node labels
         # for some unweighted kernels, construct graph
 
-        model = getattr(kernels, kernel)()
+        model = GraphKernel(kernel=kernel)
 
-        model = model.fit(g_a)
+        model.fit_transform([g_a])
 
-        dist = model.transform(g_b)
+        dist = model.transform([g_b])
 
-        return {'dist': dist}
+        return {'dist': dist[0][0]}
 
 
 n = 10
